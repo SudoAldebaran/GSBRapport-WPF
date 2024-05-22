c075fc68cca0091026b6df7ca2e1bd1e7950830e
SudoAldebaran <rayanmagniac@gmail.com>
2024-05-22 15:01:34 +0200

    Update

--- GsbRapports/GsbRapports/rapportsVisiteurs.xaml.cs
+++ GsbRapports/GsbRapports/rapportsVisiteurs.xaml.cs
@@ -23,5 +23,10 @@ namespace GsbRapports
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
